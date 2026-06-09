21661c39517fe9956d08b3917daa893ed34e7564
nanoraptor <168854832+nanoraptor@users.noreply.github.com>
2026-06-10 01:44:50 +0530

    Backup: 2026-06-10 01:44:50

---
+++ dot_local/bin/executable_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+AUR_LIST="$HOME/.config/packages/aurlist.txt"
+PKG_LIST="$HOME/.config/packages/pkglist.txt"
+mkdir -p "$HOME/.config/packages/"
+pacman -Qqem >"$AUR_LIST"
+pacman -Qqen >"$PKG_LIST"
+echo "Package lists have been backed up"
+
+chezmoi re-add
+TARGET_DIR="$HOME/.local/share/chezmoi"
+cd "$TARGET_DIR" || {
+  echo "Error: Directory '$TARGET_DIR' not found."
+  exit 1
+}
+git add .
+MSG="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "$MSG"
+git push
+echo "dotfiles have been backed up and pushed to remote"
