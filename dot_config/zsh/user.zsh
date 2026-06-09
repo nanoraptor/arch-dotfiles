1d9c73a5aafd203684b3c6e494e542cfb7727407
nanoraptor <168854832+nanoraptor@users.noreply.github.com>
2026-06-09 23:12:19 +0530

    Backup: 2026-06-09 23:12:19

---
+++ dot_config/zsh/user.zsh
@@ -0,0 +1,20 @@
+#  Startup 
+# Commands to execute on startup (before the prompt is shown)
+# Check if the interactive shell option is set
+if [[ $- == *i* ]]; then
+    # This is a good place to load graphic/ascii art, display system information, etc.
+fi
+
+#   Overrides 
+# HYDE_ZSH_NO_PLUGINS=1 # Set to 1 to disable loading of oh-my-zsh plugins, useful if you want to use your zsh plugins system 
+# unset HYDE_ZSH_PROMPT # Uncomment to unset/disable loading of prompts from HyDE and let you load your own prompts
+# HYDE_ZSH_COMPINIT_CHECK=1 # Set 24 (hours) per compinit security check // lessens startup time
+# HYDE_ZSH_OMZ_DEFER=1 # Set to 1 to defer loading of oh-my-zsh plugins ONLY if prompt is already loaded
+
+if [[ ${HYDE_ZSH_NO_PLUGINS} != "1" ]]; then
+    #  OMZ Plugins 
+    # manually add your oh-my-zsh plugins here
+    plugins=(
+        "sudo"
+    )
+fi
