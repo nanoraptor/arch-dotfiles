d4444da446937e31590c6ab9565828edede26184
Rahul M <168854832+nanoraptor@users.noreply.github.com>
2026-06-12 03:47:58 +0530

    Backup: 2026-06-12 03:47:58

--- dot_local/bin/executable_unlock_vault.sh
+++ dot_local/bin/executable_unlock_vault.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Trigger Polkit authentication
 if pkexec /usr/bin/true 2>/dev/null; then
-  # Retrieve master password from GNOME Keyring
   PASSWORD=$(secret-tool lookup title "VaultMasterPassword")
-
-  # Optional: Short delay to ensure the target window is focused
-  # after you trigger the hotkey
   sleep 0.5
-
-  # "Type" the password directly into the focused window
   echo -n "$PASSWORD" | wtype -
+  wtype -k Return
 else
   exit 1
 fi
