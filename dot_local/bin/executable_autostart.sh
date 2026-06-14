c34589198916a25156fa2b666234a1cc617307f3
Rahul M <168854832+nanoraptor@users.noreply.github.com>
2026-06-14 15:44:02 +0530

    Backup: 2026-06-14 15:44:02

--- dot_local/bin/executable_autostart.sh
+++ dot_local/bin/executable_autostart.sh
@@ -4,22 +4,17 @@
 while ! pgrep -x waybar >/dev/null; do sleep 1; done
 
 # Critical buffer: Waybar takes an additional 2-3 seconds to expose the system tray over DBus
-sleep 3
+sleep 2
 
 # Applications (Trapped by windowrulev2)
 spotify &
 discord --start-minimized &
 thunderbird &
 google-chrome-stable --profile-directory="Default" --app="https://gemini.google.com/app" &
-
-# Tray Utilities
-keepassxc &
 vicinae server &
 
 sed -i '$ s/^#\s*//' ~/.config/hypr/userprefs.conf
-
 zapzap --hideStart &
-
 for i in {1..20}; do
   if hyprctl clients -j | grep -q "com.rtosta.zapzap"; then
     hyprctl dispatch closewindow class:com.rtosta.zapzap
@@ -30,3 +25,5 @@ for i in {1..20}; do
   fi
   sleep 0.25
 done
+
+keepassxc &
