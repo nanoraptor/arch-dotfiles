b7226df15efec492a48bf66cb7e89fd6218d5389
Rahul M <168854832+nanoraptor@users.noreply.github.com>
2026-06-15 06:20:37 +0530

    Backup: 2026-06-15 06:20:37

--- dot_local/share/vicinae/scripts/Hyprland/executable_wallpaperforvicinae.sh
+++ dot_local/share/vicinae/scripts/Hyprland/executable_wallpaperforvicinae.sh
@@ -9,4 +9,4 @@
 export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export WAYLAND_DISPLAY="wayland-1"
-/home/binaryraptor/.local/lib/hyde/wallpaper.sh -S
+/home/binaryraptor/.local/lib/hyde/wallpaper.sh -SG
