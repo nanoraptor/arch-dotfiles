a9f76fa489e6357a12bd8a32b7c263759f17f052
Rahul M <168854832+nanoraptor@users.noreply.github.com>
2026-07-03 00:16:48 +0530

    Backup: 2026-07-03 00:16:48

--- dot_local/bin/executable_autostart.sh
+++ dot_local/bin/executable_autostart.sh
@@ -6,11 +6,12 @@ while ! pgrep -x waybar >/dev/null; do sleep 1; done
 # Critical buffer: Waybar takes an additional 2-3 seconds to expose the system tray over DBus
 sleep 2
 
-# Applications (Trapped by windowrulev2)
+# Applications (Trapped by windowrule)
 spotify &
 discord --start-minimized &
 thunderbird &
 firefoxpwa site launch 01KW5ZXG7VG360Z11W00D22ZCB
+zapzap --hideStart
 kdeconnect-indicator
 
 keepassxc &
